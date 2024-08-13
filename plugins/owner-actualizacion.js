4e11dc81465fad45f2e6b6815a1b6eabcbbd3405
wavenube <wavenub3@gmail.com>
2024-08-13 13:10:03 +0100

    Update owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -5,8 +5,8 @@ import axios from 'axios';
 let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
-const owner = 'BrunoSobrino';
-const repo = 'TheMystic-Bot-MD';
+const owner = 'wavenube';
+const repo = 'abyss-prueba';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
